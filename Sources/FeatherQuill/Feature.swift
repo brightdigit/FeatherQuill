44dcac008e4076461c259d090a085b6a64c20c0a
Leo Dion <leogdion@brightdigit.com>
2024-05-10 18:10:13 -0400

    adding public documentation

--- Sources/FeatherQuill/Feature.swift
+++ Sources/FeatherQuill/Feature.swift
@@ -33,6 +33,7 @@
 
   /// Set of values for the Feature.
   @Observable
+  /// Set of values for the Feature.
   public class Feature<ValueType, UserTypeValue: UserType> {
     private let featureValue: FeatureValue<ValueType>
     private let availability: FeatureAvailability<UserTypeValue>
